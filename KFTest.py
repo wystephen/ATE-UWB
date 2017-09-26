41779d7361e72584064c7b2ceacc2e353ca39a34
steve <551619855@qq.com>
2017-09-26 16:53:10 +0800

    ```

--- KFTest.py
+++ KFTest.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         src_index += 1
 
     res_pose = src_pose * 1.0
-    pf = particlefilter.ParticalFilter2D(300, 20.5, [0.1, 0.1])
+    pf = particlefilter.ParticalFilter2D(300, 2.5, [0.051, 0.051])
     pf.initial_filter(res_pose[0, :])
 
     for i in range(100):
---
+++ SingalAna.py
@@ -0,0 +1,69 @@
+# -*- coding:utf-8 -*-
+# Created by steve @ 17-9-26 下午4:26
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
+import os
+
+if __name__ == '__main__':
+    dir_name = "./Data/SignalAna/"
+    anchor_sets_file_name = dir_name + "anchor-location.csv"
+    data_dir = dir_name + "data/"
+
+    beacon_sets = np.loadtxt(anchor_sets_file_name, delimiter=',')
+    print(beacon_sets)
+
+    for file_name in os.listdir(data_dir):
+        if '.csv' in file_name:
+            print("filename :", data_dir+file_name)
+            tmp_data = np.genfromtxt(data_dir+file_name,
+                                     delimiter=',',
+                                     filling_values=-1.0)
+
+            tag_pose = np.zeros(3)
+            tag_pose[0] = float(file_name.split('-')[1])
+            tag_pose[1] = float(file_name.split('-')[2].split('.csv')[0])
+            tag_pose[2] = 1.7
+
+            real_distance = np.zeros(beacon_sets.shape[0])
+
+            plt.figure()
+            for i in range(beacon_sets.shape[0]):
+                real_distance[i] = np.linalg.norm(
+                    beacon_sets[i, :] - tag_pose
+                )
+                plt.plot(tmp_data[:,i]-real_distance[i],'*',label=str(i))
+            plt.grid()
+            plt.legend()
+            plt.title(file_name)
+            plt.ylim(0.0,10.0)
+            plt.savefig(file_name.split('.csv')[0].split('T105-')[1]+'.jpg')
+            # plt.show()
+            print(real_distance)
+
+
